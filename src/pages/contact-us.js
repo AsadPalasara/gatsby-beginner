e992062db8b5a08965043fff7a0cf84224aa97cb
Asadabbas Palasara <asad.palasara@multidots.com>
2019-03-18 16:03:28 +0530

    update

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -14,29 +14,20 @@ const Contact = props => (
     <div className="container">
       <section id="contact" className="contact-form">
         <h2>GET IN TOUCH</h2>
-        <form
-          className="row"
-          name="contac-form"
-          method="post"
-          action="/success/" 
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-        >
-          <input type="hidden" name="bot-field" />
-
+        <form name="contact-form" className="row" method="post" action="/success/" data-netlify="true" data-netlify-honeypot="bot-field">
+          <input type="hidden" name="bot-field" /> 
           <div className="field col-md-6">
             <input type="text" name="name" id="name" required />
             <label htmlFor="name">Name</label>
           </div>
           <div className="field col-md-6">
-            <input type="text" name="email" id="email" required />
+            <input type="email" name="email" id="email" required />
             <label htmlFor="email">Email</label>
           </div>
           <div className="field col-12">
             <textarea name="message" id="message" rows="6" required />
             <label htmlFor="message">Message</label>
-          </div>
-          <div data-netlify-recaptcha="true"></div>
+          </div> 
           <div className="action">
             <input type="submit" value="Send Message" className="draw-border btn btn-outline-primary btn-outline-dark clear-fix stretched-link"/>
             <input type="reset" value="Clear" className="draw-border btn btn-outline-primary btn-outline-dark clear-fix stretched-link" />
